1d34ecd4aa5bcf169a7eb604e4fbf811a80f0e47
Ewing <e.singasara@alustudent.com>
2024-09-06 15:58:02 -0700

    0

--- regular_expressions/0-simply_match_school.rb
+++ regular_expressions/0-simply_match_school.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-# This script prints all occurrences of the word "School" from the argument
-
-# Match the word "School" and print all occurrences
 puts ARGV[0].scan(/School/).join
