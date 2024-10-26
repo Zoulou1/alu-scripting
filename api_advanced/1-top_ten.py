953aebef599fc57f56987cbb39336c0c507f8c25
Ewing <e.singasara@alustudent.com>
2024-10-26 10:09:49 +0000

    1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -2,17 +2,19 @@
 """DOCS"""
 import requests
 
-
 def top_ten(subreddit):
     """Docs"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
-        .format(subreddit)
-    headers = headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers)
-
+    reddit_url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    
+    # Fetch subreddit data with redirects disabled
+    response = requests.get(reddit_url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
-        data = response.json()['data']
-        for post in data['children'][:10]:
-            print(post['data']['title'])
+        data = response.json().get('data', {}).get('children', [])
+        for post in data[:10]:
+            print(post['data'].get('title'))
+        return "OK"
     else:
         print(None)
+        return None
