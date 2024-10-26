583b1be40c2d5696297a89a114206fabc7b51624
Ewing <e.singasara@alustudent.com>
2024-10-26 10:04:36 +0000

    1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
+"""DOCS"""
 import requests
 
+
 def top_ten(subreddit):
-    # Define the URL for the Reddit API
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    # Set the User-Agent to avoid blocking by Reddit's API
-    headers = {'User-Agent': 'python:subreddit.hot.posts:v1.0 (by /u/yourusername)'}
-    
-    try:
-        # Make a GET request to the Reddit API
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        # Check if the request was successful and there was no redirect
-        if response.status_code == 200:
-            data = response.json().get('data', {}).get('children', [])
-            # Print the titles of the first 10 posts
-            for post in data[:10]:
-                print(post['data'].get('title'))
-        else:
-            # Print None if the subreddit is invalid or redirected
-            print(None)
-    except Exception as e:
+    """Docs"""
+    reddit_url = "https://www.reddit.com/r/{}/hot.json" \
+        .format(subreddit)
+    headers = headers = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()['data']
+        for post in data['children'][:10]:
+            print(post['data']['title'])
+    else:
         print(None)
